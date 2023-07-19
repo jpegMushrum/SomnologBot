c88e1ffad7560882fc300c40fe4736b75eb849d9
jpegMushrum <artem070705@yandex.ru>
2023-07-19 18:12:48 +0300

    Start, help commands added

--- README.md
+++ README.md
@@ -1,18 +1,17 @@
 # Somnolog bot
-
-## Folder structure
-- src for sources (scripts, token)
-- db for database (MySQL)
-
 ## Functional
 - Add dream, set it's type, description
 - See your history of dreams
 - See statistic
-- Set notifications, bot will ask you about the dream at a certain time, so you won't forget to add dream
 
-## Usage
+## Setting Up
 1. You need your own bot token, get it there: https://t.me/BotFather.
 2. Make token.txt with only your bot token in the src folder.
-3. Start the console in the main folder (SomnologBot) and start `python -m venv venv` (Windows) | `python3 -m venv venv` (Linux) to install virtual environment.
-4. Install aiogram to your virtual environment.
-5. Windows: start the start_w.bat | Linux: start the start_l.sh.
+3. Make dbinfo.py with info about your database. Fields: user, password, host, db_name.
+4. Start the console in the main folder (SomnologBot) and start `python -m venv venv` (Windows) | `python3 -m venv venv` (Linux) to install virtual environment.
+5. Install requirements to your virtual environment. Activate venv through `call venv/Scripts/activate` (Windows) | `source venv/bin/activate` (Linux).Use `pip install -r requirements.txt`.
+6. to start use `start_w.bat` (Windows) | `bash ./start_l.sh` (Linux).
+
+## Technology Stack
+- MySQL is used for database.
+- Aiogram is used for Bot.
--- src/bot.py
+++ src/bot.py
@@ -9,13 +9,29 @@ token = "".join(token.split())
 bot = Bot(token=token)
 dp = Dispatcher(bot)
 
+msg_on_start = 'Hello, it\'s a bot for tracking your dreams. You can add new dream, see your history of dreams, ' \
+               'see statistic or set time notifications. Use shown commands: \n/add - Let you add new dream. ' \
+               '\n/history - Shows your numbered dream. \n /statistic - Shows your statistic.\n ' \
+               '/help - Show list of commands again.'
+
+msg_on_help = 'Available commands: \n/add - Let you add new dream. \n/history - Shows your numbered dream. \n ' \
+              '/statistic - Shows your statistic.\n /help - Show list of commands again.\n ' \
+              'Call some to see information about this command.'
 
 async def on_startup(_):
     print('Bot is now running')
 
+@dp.message_handler(commands=['start'])
+async def command_start(message: types.Message):
+    await bot.send_message(message.from_user.id, msg_on_start)
+
+@dp.message_handler(commands=['help'])
+async def command_help(message: types.Message):
+    await bot.send_message(message.from_user.id, msg_on_help)
+
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    await bot.send_message(message.from_user.id, message.text)
+    await bot.send_message(message.from_user.id, message.from_user.id)
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
